3e21a91d809422850240671f15458de446a80f27
Nirmal Aryal <88726016+neeraryal@users.noreply.github.com>
2026-07-09 00:44:34 +0530

    Time: 2 ms (15.03%), Space: 9.3 MB (10.94%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     bool isValid(string s) 
     {
+        unordered_map<char,char> mapp= {{'}','{'},{']','['},{')','('}};
+        stack<char>st;
+
+        for(int i=0 ; i< s.size();i++)
+        {
+            if(s[i]=='(' || s[i] == '{' || s[i] == '[')  
+                st.push(s[i]);
+            else
+            {
+                if(st.empty() || st.top() != mapp[s[i]]) return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+
+
+
+/*
+    bool isValid(string s) 
+    {
         stack<char> st;
         int i=0 ;
         while(i<s.size())
@@ -44,4 +66,4 @@ public:
 
         return true;
     }
-};
+*/
