07c228be0988ff73a7e755a63599db3527610c47
Nirmal Aryal <88726016+neeraryal@users.noreply.github.com>
2026-07-11 20:15:02 +0530

    Time: 0 ms (100%), Space: 8 MB (76.69%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) 
+    {
+        if (s.size() != goal.size())
+            return false;
+
+        string temp= goal+goal;
+        return temp.find(s) != string::npos;
+    }
+};
